a3878995dcca227bedb9d5530961bdc3acbd64de
FatimaAflous <117607446+FatimaAflous@users.noreply.github.com>
2024-05-22 01:09:50 +0100

    Update 2 HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello  World 3yyt barakaaaaaaaa !");
+        System.out.println("Hello  World 3yyt barakaaaaaaaa 3lya !");
     }
 }
